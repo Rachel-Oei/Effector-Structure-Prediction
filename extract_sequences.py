80bedd5a3b2f52db44612f6fe5bd7ee8a29be219
Rachel-Oei <oeirachel@yahoo.com>
2026-07-21 12:12:57 +0000

    write script

--- extract_sequences.py
+++ extract_sequences.py
@@ -1,30 +1,41 @@
-import os 
 from Bio.PDB.MMCIF2Dict import MMCIF2Dict
 
 # Create a folder called cif_fasta
-text_file="/home/rachel/cif/PDB_ID_list2.txt"
+text_file="/home/rachel/cif/PDB_ID_list.txt"
 input_directory="/home/rachel/cif/cif_downloads/"
 output_directory="/home/rachel/cif/cif_fasta/"
 
 """
-MMCIF2Dict dictionary looks like: 
-d["_struct_asym.id"]         = ["A", "B"]
-d["_struct_asym.entity_id"]  = ["1", "1"]
-d["_entity_poly.entity_id"]  = ["1"]
-d["_entity_poly.pdbx_seq_one_letter_code_can"] = ["MKVLAT...GYQG"]
+example dictionary output: 
 
+dictionary["_entity_poly.pdbx_strand_id"]
+# ['A', 'B']
+dictionary["_entity_poly.pdbx_seq_one_letter_code_can"]
+# ['IVGGTSASAGD...RSFIDTYA', 'GAR']
 """
-for filename in os.listdir(input_directory):
-    protein_id=filename[:4] # to remove .cif extension
-    cif_path=input_directory+f"{protein_id}.cif"
 
-    dictionary=MMCIF2Dict(cif_path)
+def get_sequence_by_chain(cif_file, chain):
+    d = MMCIF2Dict(cif_file)
 
-    entity_to_seq = dict(zip(
-        dictionary["_entity_poly.entity_id"],
-        dictionary["_entity_poly.pdbx_seq_one_letter_code_can"]
-    ))
+    strand_ids = d["_entity_poly.pdbx_strand_id"]
+    sequences = d["_entity_poly.pdbx_seq_one_letter_code_can"]
 
-    seq = entity_to_seq.get(str(entity_id))
-    return seq.replace("\n", "") if seq else None
+    for strand_id, sequence in zip(strand_ids, sequences):
+        if chain in strand_id.split(","):
+            return sequence.replace("\n", "")
 
+with open("/home/rachel/cif/PDB_ID_list2.txt") as f:
+    protein_ID_list = [line.strip() for line in f if line.strip()]
+
+for protein_id in protein_ID_list:
+    protein=protein_id[:4]
+    chain=protein_id[5]
+    cif_file=f"{input_directory}{protein}.cif"
+    sequence = get_sequence_by_chain(cif_file, chain)
+    output_file = f"{output_directory}{protein}.fasta"
+
+    with open(output_file, "w") as f:
+        f.write(f">{protein}\n")
+        f.write(sequence + "\n")
+
+    print(f"Created {output_file}")
