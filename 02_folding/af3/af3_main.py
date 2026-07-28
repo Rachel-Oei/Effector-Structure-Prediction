0326d4047866960ae17a6904acd62f54aa5aba20
Rachel-Oei <oeirachel@yahoo.com>
2026-07-28 01:05:45 +0000

    improve main

--- 02_folding/af3/af3_main.py
+++ 02_folding/af3/af3_main.py
@@ -11,11 +11,14 @@ from create_json import create_json
 
 def main():
     cif_directory = "/home/rachel/01_prepare_cif"
+    folding_directory="/home/rachel/02_folding"
+
     input_text_entity = cif_directory+"/input_pdb_lists/pdb_list_entity.txt"
     input_text_chain = cif_directory+"/input_pdb_lists/pdb_list_chain.txt"
     fasta_dir=cif_directory+"/cif_fasta"
-    json_output_dir="/home/rachel/02_folding/af3/json"
-    af3_script="/home/rachel/02_folding/af3/run_all_af3.sh"
+
+    json_output_dir=folding_directory+"/af3/json"
+    af3_script=folding_directory+"/af3/run_all_af3.sh"
 
     create_directory(json_output_dir)
     create_json(input_text_entity, input_text_chain, fasta_dir, json_output_dir)
