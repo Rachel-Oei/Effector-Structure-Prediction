ad48fc5f2f83f3d0ab04dc06f6dd68f1ede9ce2f
Rachel-Oei <oeirachel@yahoo.com>
2026-07-28 01:09:50 +0000

    improve function json

--- 02_folding/af3/create_json.py
+++ 02_folding/af3/create_json.py
@@ -6,12 +6,31 @@ sys.path.append("/home/rachel/01_prepare_cif")
 from prepare_cif_models import pdb_text_to_list
 
 def create_json (input_text_entity, input_text_chain, fasta_dir, output_dir):
+    """
+    Reads fasta file for each protein, creates json file in the format of:
+    {
+      "name": "2PV7_1",
+      "sequences": [
+	{
+	  "protein": {
+            "id": ["A"],
+            "sequence": "GMRESYANENQFGFKTINSDIHKIVIVGGYGKLGGLFARYLRASGYPISILDREDWAVAESILANADVVIVSVPINLTLETIERLKPYLTENMLLADLTSVKREPLAKMLEVHTGAVLGLHPMFGADIASMAKQVVVRCDGRFPERYEWLLEQIQIWGAKIYQTNATEHDHNMTYIQALRHFSTFANGLHLSKQPINLANLLALSSPIYRLELAMIGRLFAQDAELYADIIMDKSENLAVIETLKQTYDEALTFFENNDRQGFIDAFHKVRDWFGDYSEQFLKESRQLLQQANDLKQG"
+	  }
+	}
+      ],
+      "modelSeeds": [1],
+      "dialect": "alphafold3",
+      "version": 1
+    }
 
+    Output name is e.g 2PV7_1.json
+
+    """
     list_chain = pdb_text_to_list(input_text_chain)
     list_entity = pdb_text_to_list(input_text_entity)
 
     for entity, chain in zip(list_entity, list_chain):
-        protein=entity[:4] #just 1FN8
+        protein=entity[:4] # protein ID, e.g 1FN8
         entity=entity[5]
         chain=chain[5]
 
