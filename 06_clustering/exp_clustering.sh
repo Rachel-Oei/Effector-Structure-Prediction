5e4f23633e4362de0188c9705238c9dccdee1f7b
Rachel-Oei <oeirachel@yahoo.com>
2026-07-29 12:12:08 +0000

    af3 and exp clustering bash scripts

---
+++ 06_clustering/exp_clustering.sh
@@ -0,0 +1,22 @@
+# Will run on GPU 0
+
+HOME_DIR="/home/rachel"
+mkdir -p "${HOME_DIR}/06_clustering"
+cd "${HOME_DIR}/06_clustering"
+
+# Download Foldseek through:
+# wget https://mmseqs.com/foldseek/foldseek-linux-gpu.tar.gz
+# tar xvzf foldseek-linux-gpu.tar.gz
+
+export PATH=$(pwd)/foldseek/bin/:$PATH
+
+# Move af3.cif files to clustering folder 
+EXP_CIF="/home/rachel/01_prepare_cif/cif_single_chain"
+EXP_TMP="/linuxhome/tmp/${USER}/experimental_structures/"
+FDSK_OUT="/home/rachel/06_clustering/experimental/foldseek_output"
+
+mkdir -p ${EXP_TMP}
+mkdir -p ${FDSK_OUT}
+
+#Run foldseek on GPU 0
+CUDA_VISIBLE_DEVICES=0 foldseek easy-cluster ${EXP_CIF} "${FDSK_OUT}/experimental_clusters" ${EXP_TMP}
