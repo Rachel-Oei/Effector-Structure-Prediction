a042e7b696998941923137f06470178c27183a08
Rachel-Oei <oeirachel@yahoo.com>
2026-07-29 15:10:13 +0000

    try neighbors 15

--- 06_clustering/tm_matrix.py
+++ 06_clustering/tm_matrix.py
@@ -34,7 +34,7 @@ def plot_umap (matrix):
 
     reducer = umap.UMAP(
         metric='precomputed',
-        n_neighbors=50,
+        n_neighbors=15,
         min_dist=0.0,
         random_state=42
     )
