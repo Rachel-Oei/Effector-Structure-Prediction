ce1a5f346efa93254539afcee4a19e8b06d72b28
Rachel-Oei <oeirachel@yahoo.com>
2026-07-31 12:27:46 +0000

    add scripts to cut multi-fasta to single-fasta

---
+++ 07_fold_all/prepare_fasta.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+MULTI_FASTA_DIR="/home/rachel/07_fold_all/foec_2/multi_fasta"
+SINGLE_FASTA_DIR="/home/rachel/07_fold_all/foec_2/single_fasta"
+
+mkdir -p "$SINGLE_FASTA_DIR"
+
+for multi_fasta in "$MULTI_FASTA_DIR"/*.fasta; do
+
+    # Get filename without .fasta
+    name=$(basename "$multi_fasta" _08_putative_effectors_protein.fasta)
+
+    awk -v output_dir="$SINGLE_FASTA_DIR" -v name="$name" '
+    /^>/ {
+        # Get first identifier after >
+        protein_id = $2
+
+        outfile = output_dir "/" name "_" protein_id ".fasta"
+    }
+
+    {print > outfile}
+
+    ' "$multi_fasta"
+
+done
+
+echo "Finished splitting FASTA files."
---
+++ 07_fold_all/prepare_fasta_foec_2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+MULTI_FASTA_DIR="/home/rachel/07_fold_all/foec_2/multi_fasta"
+SINGLE_FASTA_DIR="/home/rachel/07_fold_all/foec_2/single_fasta"
+
+mkdir -p "$SINGLE_FASTA_DIR"
+
+for multi_fasta in "$MULTI_FASTA_DIR"/*.fasta; do
+
+    # Get filename without .fasta
+    name=$(basename "$multi_fasta" _08_putative_effectors_protein.fasta)
+
+    awk -v output_dir="$SINGLE_FASTA_DIR" -v name="$name" '
+    /^>/ {
+        # Get first identifier after >
+        protein_id=$1
+        sub(/^>/, "", protein_id)
+        gsub(/:/, "_", protein_id)
+
+        outfile = output_dir "/" name "_" protein_id ".fasta"
+    }
+
+    {print > outfile}
+
+    ' "$multi_fasta"
+
+done
+
+echo "Finished splitting FASTA files."
