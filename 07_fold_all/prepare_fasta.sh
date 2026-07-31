f0e6cac91cb399c5b4a260c93131ff725f23eb79
Rachel-Oei <oeirachel@yahoo.com>
2026-07-31 13:55:42 +0000

    cut pepttides script

--- 07_fold_all/prepare_fasta.sh
+++ 07_fold_all/prepare_fasta.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-MULTI_FASTA_DIR="/home/rachel/07_fold_all/foec_2/multi_fasta"
-SINGLE_FASTA_DIR="/home/rachel/07_fold_all/foec_2/single_fasta"
+MULTI_FASTA_DIR="/home/rachel/07_fold_all/effector_p/multi_fasta"
+SINGLE_FASTA_DIR="/home/rachel/07_fold_all/effector_p/single_fasta"
 
 mkdir -p "$SINGLE_FASTA_DIR"
 
 for multi_fasta in "$MULTI_FASTA_DIR"/*.fasta; do
 
     # Get filename without .fasta
-    name=$(basename "$multi_fasta" _08_putative_effectors_protein.fasta)
+    name=$(basename "$multi_fasta" __protein_sequences.fasta)
 
     awk -v output_dir="$SINGLE_FASTA_DIR" -v name="$name" '
     /^>/ {
