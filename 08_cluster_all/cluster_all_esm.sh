df4d4d54bf83e655e5836ff55bc93c97f8b095b6
Rachel-Oei <oeirachel@yahoo.com>
2026-08-01 09:59:51 +0000

    add cluster all for esm effector p scripts

---
+++ 08_cluster_all/cluster_all_esm.sh
@@ -0,0 +1,59 @@
+# Will run on GPU 0
+
+HOME_DIR="/home/rachel"
+mkdir -p "${HOME_DIR}/08_cluster_all"
+cd "${HOME_DIR}/08_cluster_all"
+
+# Download Foldseek through:
+# wget https://mmseqs.com/foldseek/foldseek-linux-gpu.tar.gz
+# tar xvzf foldseek-linux-gpu.tar.gz
+
+export PATH=$(pwd)/foldseek/bin/:$PATH
+
+# Move esm.pdb files to clustering folder 
+ESM_DIR="/home/rachel/07_fold_all/effector_p/esm/esmfold-results"
+ESM_PDB="/home/rachel/08_cluster_all/effector_p/esm/pdb"
+ESM_TMP="/linuxhome/tmp/${USER}/esm/"
+FDSK_OUT="/home/rachel/08_cluster_all/effector_p/esm/foldseek_output"
+
+mkdir -p ${ESM_PDB}
+mkdir -p ${ESM_TMP}
+mkdir -p ${FDSK_OUT}
+
+for folder in "$ESM_DIR"/*; do
+
+    id=$(basename "$folder")
+    predicted=("$folder"/*.pdb)
+
+    if [ ! -f "${predicted[0]}" ]; then
+        echo "Warning: no PDB found for ${id}"
+        continue
+    fi
+
+    pdb_file="${predicted[0]}"
+    destination="$ESM_PDB/${id}.pdb"
+
+    # Check whether this folder has already been copied
+    if [ -f "$destination" ]; then
+        echo "Skipping ${id}: already moved"
+        continue
+    fi
+
+    cp "$pdb_file" "$destination"
+
+    echo "Moved $(basename "$pdb_file") → ${id}.pdb"
+
+done
+
+# Run foldseek easy-cluster on GPU 0 
+CUDA_VISIBLE_DEVICES=0 foldseek easy-cluster ${ESM_PDB} "${FDSK_OUT}/esm_clusters" ${ESM_TMP}
+
+# Run foldseek easy-search on GPU 0 
+CUDA_VISIBLE_DEVICES=0 foldseek easy-search \
+    ${ESM_PDB} \
+    ${ESM_PDB} \
+    ${FDSK_OUT}/esm_foldseek_results.tsv \
+    ${ESM_TMP} \
+    --format-output "query,target,alnlen,alntmscore,qtmscore,ttmscore,rmsd"
+
+# Use tmscore normalized by alignment length: alntmscore
