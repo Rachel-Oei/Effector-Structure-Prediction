f0346be73e076a1e0e91e5b4c2971a9f257eac3f
Rachel-Oei <oeirachel@yahoo.com>
2026-08-03 13:18:59 +0000

    edit fold_all_scripts

---
+++ 07_fold_all/af3/create_json.py
@@ -0,0 +1,74 @@
+import json
+import sys
+import os 
+
+sys.path.append("/home/rachel/01_prepare_cif")
+from prepare_cif_models import create_directory
+
+folding_directory="/home/rachel/07_fold_all/effector_p"
+fasta_dir="/home/rachel/07_fold_all/effector_p/single_cut_fasta"
+
+output_dir=folding_directory+"/af3/json"
+create_directory(output_dir)
+
+def create_json (fasta_dir, output_dir):
+    """
+    Reads fasta file for each protein, creates json file in the format of:
+    {
+      "name": "2PV7_1",
+      "sequences": [
+	{
+	  "protein": {
+            "id": ["A"],
+            "sequence": "GMRESYANENQFGFKTINSDIHKIVIVGGYGKLGGLFARYLRASGYPISILDREDWAVAESILANADVVIVSVPINLTLETIERLKPYLTENMLLADLTSVKREPLAKMLEVHTGAVLGLHPMFGADIASMAKQVVVRCDGRFPERYEWLLEQIQIWGAKIYQTNATEHDHNMTYIQALRHFSTFANGLHLSKQPINLANLLALSSPIYRLELAMIGRLFAQDAELYADIIMDKSENLAVIETLKQTYDEALTFFENNDRQGFIDAFHKVRDWFGDYSEQFLKESRQLLQQANDLKQG"
+	  }
+	}
+      ],
+      "modelSeeds": [1],
+      "dialect": "alphafold3",
+      "version": 1
+    }
+
+    Output name is e.g 2PV7_1.json
+
+    """    
+    for fasta_file in os.listdir(fasta_dir):
+
+        if fasta_file.endswith(".fasta"):
+            pdb_id = fasta_file.replace(".fasta", "")
+        else:
+            continue 
+
+        fasta_path = os.path.join(fasta_dir, fasta_file)
+
+        with open(fasta_path) as f:
+            sequence_lines = []
+            for line in f:
+                if not line.startswith(">"):
+                    sequence_lines.append(line.strip())
+            sequence = "".join(sequence_lines)
+
+        json_data={
+            "name":f"{pdb_id}",
+            "sequences": [
+            {
+                "protein": {
+                "id": ["A"],
+                "sequence": sequence
+                }
+            }
+            ],
+            "modelSeeds": [1],
+            "dialect": "alphafold3",
+            "version": 1
+        }
+        
+        output_file = f"{output_dir}/{pdb_id}.json"
+
+        # Write the json file 
+        with open(output_file, "w") as f:
+            json.dump(json_data, f, indent=2)
+
+        print(f"{pdb_id}.json created")
+
+create_json(fasta_dir, output_dir)
---
+++ 07_fold_all/af3/run_all_af3_effector_p.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# This script uses GPU 0. Runs AF3 for each protein and appends runtime in a .csv file. 
+
+AF3_FOLDER="/home/rachel/07_fold_all/effector_p/af3"
+
+AF3_DIR="/home/rachel/02_folding/af3/run_alphafold.sh"
+JSON_DIR="${AF3_FOLDER}/json"
+MODEL_DIR="/home/rachel/02_folding/af3/alphafold-models-3.0.3"
+DB_DIR="/net/leca/linuxhome/alphafold/alphafold-db-3.0.3"
+OUTPUT_DIR="${AF3_FOLDER}/alphafold3-3.0.3/output"
+RUNTIME_CSV="${AF3_FOLDER}/af3_runtime.csv"
+
+mkdir -p ${OUTPUT_DIR}
+
+for json_file in ${JSON_DIR}/*.json
+do
+    protein_identity=$(basename "$json_file" .json)
+
+    summary_file="${OUTPUT_DIR}/${protein_identity}/${protein_identity}_summary_confidences.json"
+
+    # Skip if AF3 output already exists
+    if [ -f "$summary_file" ]; then
+        echo "Skipping ${protein_identity}: already completed"
+        continue
+    fi
+
+    echo "Starting AF3 for ${json_file}"
+
+    # Collect runtime 
+    start=$(date +%s)
+
+    # Use specifically GPU 0 
+    APPTAINERENV_CUDA_VISIBLE_DEVICES=0 ${AF3_DIR} \
+      --json_path=${json_file} \
+      --model_dir=${MODEL_DIR} \
+      --db_dir=${DB_DIR} \
+      --output_dir=${OUTPUT_DIR}
+
+    end=$(date +%s)
+    runtime=$((end - start))
+    
+    echo "Finished ${json_file}"
+    
+    echo "${protein_identity},${runtime}" >> $RUNTIME_CSV
+
+done
