93c41bf0aa01e61dd81fa47678372928766cced1
Rachel-Oei <oeirachel@yahoo.com>
2026-08-26 09:29:46 +0000

    add tree_construction files

---
+++ 08_cluster_all/tree_construction_main.py
@@ -0,0 +1,9 @@
+from tree_construction_models import tree_construction
+
+def main():
+
+    tree_construction("effector_p", "esm")
+    tree_construction("foec_2", "esm")
+
+if __name__ == "__main__":
+    main()
